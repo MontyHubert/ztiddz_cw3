7bdd2661a7e39a2e75d7eef32776a77c73f93c83
Hubert Maciakiewicz <95583560+MontyHubert@users.noreply.github.com>
2023-12-04 13:40:02 +0100

    added comments

--- cw3/index.js
+++ cw3/index.js
@@ -1,43 +1,48 @@
-// Requiring module
+
 const express = require("express");
 var path = require('path');
 
 const app = express();
 
+// Funkcja do autoryzacji klienta
 function authentication(req, res, next) {
-	var authheader = req.headers.authorization;
-	console.log(req.headers);
+    var authheader = req.headers.authorization; // Pobranie nagłówka autoryzacji
 
-	if (!authheader) {
-		var err = new Error('You are not authenticated!');
-		res.setHeader('WWW-Authenticate', 'Basic');
-		err.status = 401;
-		return next(err)
-	}
+    console.log(req.headers); // Wyświetlenie wszystkich nagłówków
 
-	var auth = new Buffer.from(authheader.split(' ')[1],
-	'base64').toString().split(':');
-	var user = auth[0];
-	var pass = auth[1];
+    // Sprawdzenie, czy nagłówek jest obecny
+    if (!authheader) {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err);
+    }
 
-	if (user == '' && pass == '') {
+    // Dekodowanie nagłówka z base64
+    var auth = new Buffer.from(authheader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
 
-		// If Authorized user
-		next();
-	} else {
-		var err = new Error('You are not authenticated!');
-		res.setHeader('WWW-Authenticate', 'Basic');
-		err.status = 401;
-		return next(err);
-	}
-
+    // Sprawdzenie, czy użytkownik i hasło są puste
+    if (user == '' && pass == '') {
+        // Jeśli użytkownik jest autoryzowany, przechodzi do kolejnej warstwy
+        next();
+    } else {
+        // Jeśli niepoprawne dane autoryzacyjne, zwraca błąd
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err);
+    }
 }
 
-// First step is the authentication of the client
-app.use(authentication)
+// Pierwszym krokiem jest autoryzacja klienta przy użyciu funkcji 'authentication'
+app.use(authentication);
+
+// Ustawienie zasobów statycznych z katalogu 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Server setup
+// Konfiguracja serwera na porcie 3000
 app.listen((3000), () => {
-	console.log("Server is Running");
+    console.log("Server is Running");
 })
